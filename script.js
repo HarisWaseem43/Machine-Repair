a731a11e1d35a757139713dc02a2217674f23468
Haris Bin Waseem <hariswaseem430@gmail.com>
2024-03-17 20:37:35 +0500

    Some Changes

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleBtn = document.querySelector('.toggle-btn');
-    const navItems = document.querySelector('.nav_Container_Items');
-  
-    toggleBtn.addEventListener('click', function() {
-      navItems.classList.toggle('active');
-    });
-  });
